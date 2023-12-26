0afa2f191f10b35167e437638c1b60043c1a6b69
VigneshDhakshnamoorthy <vigneshdhakshnamoorthy@gmail.com>
2023-12-26 17:46:25 +0530

    print

--- database/database.py
+++ database/database.py
@@ -5,7 +5,6 @@ def init_db(app):
     database_folder = os.path.join(os.path.abspath(os.path.dirname(__file__)), "db")
     os.makedirs(database_folder, exist_ok=True)
     database_file_path = os.path.join(database_folder, "user.db")
-    print(database_file_path)
     app.config["SECRET_KEY"] = "ces_secret_key"
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{database_file_path}"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
